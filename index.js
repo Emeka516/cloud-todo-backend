bf8d2bd34a983a362a46b703b9cb5e3529f986b2
Nwaeke Victor <victornwaeke12@gmail.com>
2026-03-29 20:59:22 +0100

    fix app.listen to bind to 0.0.0.0

--- index.js
+++ index.js
@@ -89,6 +89,6 @@ app.put("/tasks/:id", async (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
